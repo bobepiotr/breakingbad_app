b7170b09bbaaec6baca7c1149145cbd4331bf98f
Piotr <bobepiotr@gmail.com>
2021-04-18 17:59:00 +0200

    Add app menu

--- app/src/main/java/com/example/bb_app/MainActivity.kt
+++ app/src/main/java/com/example/bb_app/MainActivity.kt
@@ -1,26 +1,19 @@
 package com.example.bb_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private var counter: Int = 0
-    private var textView: TextView? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        this.textView = findViewById(R.id.counter_text)
     }
 
-    fun incrementCounter(view: View) {
-        this.counter++
-
-        this.textView?.text =
-            if (this.counter != 1) "Button clicked $counter times"
-            else "Button clicked $counter time"
+    fun openCharacterList(view: View) {
+        val intent = Intent(this, CharacterList::class.java)
+        startActivity(intent)
     }
 }
