bf37f2caf73072fe11a372d2a5ccf61612ac5e0a
Piotr <bobepiotr@gmail.com>
2021-04-23 16:46:16 +0200

    Add empty activity

---
+++ app/src/main/java/com/example/bb_app/CharacterDetails.kt
@@ -0,0 +1,11 @@
+package com.example.bb_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CharacterDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_details)
+    }
+}
